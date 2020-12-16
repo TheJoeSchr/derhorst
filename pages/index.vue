4be87206034206ae6c1e779f012b999c19aea268
Joe Schr <joe.vandelay@gmail.com>
2020-12-16 13:13:53 +0100

    fix: padding center

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="full">
-    <div class="grid grid-cols-1 lg:grid-cols-6 gap-4 divide-black divide-x lg:gap-8 m-5 ">
-      <div class="p-4 col-span-1 lg:col-span-3 content-center content-between justify-items-center">
+    <div class="grid grid-cols-1 lg:grid-cols-6 gap-4 divide-black divide-x lg:gap-8 m-5">
+      <div class="p-4 col-span-1 lg:col-span-3 px-8 ">
         <h1 class="title">der_horst</h1>
         <img src="~/assets/video.png" />
       </div>
-      <div class="ter lg:col-span-2"><img src="~/assets/text-m-t.png" /><img src="~/assets/text-m-b.png" /></div>
+      <div class="text-center lg:col-span-2 px-8"><img src="~/assets/text-m-t.png" /><img src="~/assets/text-m-b.png" /></div>
       <div class="p-4 text-center place-content-between">
         <img src="~/assets/legs.png" />
         <div class="links">
@@ -35,6 +35,9 @@ export default Vue.extend({})
 @apply min-h-screen flex justify-center items-center text-center mx-auto;
 }
 */
+.marign-zero{
+  margin: 0 auto;
+}
 .full {
   margin: 0 auto;
   min-height: 100vh;
